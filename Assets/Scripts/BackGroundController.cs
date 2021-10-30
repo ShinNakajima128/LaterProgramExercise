54ab212c2b9f6c868d695f62f370e4ba5502dd7d
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-10-31 01:24:01 +0900

    背景をクロスフェードさせる機能を追加した

--- Assets/Scripts/BackGroundController.cs
+++ Assets/Scripts/BackGroundController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class BackGroundController : MonoBehaviour
 {
@@ -9,20 +10,40 @@ public class BackGroundController : MonoBehaviour
     Image[] m_backgrounds = default;
 
     [SerializeField]
-    Sprite[] m_backgroundTypes = default; 
+    Sprite[] m_backgroundTypes = default;
 
+    int m_currentBackground = 0;
     Animator m_anim = default;
+    public static Action BackgroundAnim = default;
 
     void Start()
     {
         m_anim = GetComponent<Animator>();
     }
 
-    public void Crossfade(int current, int backgroundType)
+    public void Setup(int backgroundType)
     {
-        if (current == 1)
-        {
+        m_backgrounds[0].sprite = m_backgroundTypes[backgroundType];
+    }
 
+    public void Crossfade(int backgroundType)
+    {
+        if (m_currentBackground == 0)
+        {
+            m_backgrounds[1].sprite = m_backgroundTypes[backgroundType];
+            m_anim.Play("BackgroundAnimation1");
+            m_currentBackground = 1;
         }
+        else
+        {
+            m_backgrounds[0].sprite = m_backgroundTypes[backgroundType];
+            m_anim.Play("BackgroundAnimation2");
+            m_currentBackground = 0;
+        }
+    }
+
+    public void OnAnimationFinish()
+    {
+        BackgroundAnim?.Invoke();
     }
 }
